0f3294303eaafc7e584191192be008dda886b040
Mateus Oliveira <mthenriqueo@gmail.com>
2024-09-15 15:10:30 -0300

    chore: initial config

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { app } from "./app";
+import { env } from "./envs";
+
+app.listen({ 
+    port: env.PORT,
+    host: '0.0.0.0'
+}).then(() => {
+    console.log(`Server running on port ${env.PORT}`)
+});
