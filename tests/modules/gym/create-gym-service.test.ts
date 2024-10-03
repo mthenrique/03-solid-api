d0410752a41522b774e5c9d60176f0ac8e7e3fc6
Mateus Oliveira <mthenriqueo@gmail.com>
2024-10-02 22:16:58 -0300

    test: add new test for create gym

--- tests/modules/gym/create-gym-service.test.ts
+++ tests/modules/gym/create-gym-service.test.ts
@@ -1,7 +1,8 @@
 import { GymsRepository } from "@/infra/database/repositories/gyms-repository";
+import { ExceptionError } from "@/infra/errors/exception-error";
 import CreateGymService from "@/modules/gym/services/create-gym-service";
 import GymsRepositoryInMemory from "tests/in-memory-repositories/gyms-repository-in-memory";
-import { describe, it, expect, beforeEach } from "vitest";
+import { describe, it, expect, beforeEach, vi } from "vitest";
 
 let gymsRepository: GymsRepository
 let createGymService: CreateGymService
@@ -29,5 +30,19 @@ describe('CreateGymService', () => {
     expect(createdGym.phone).toEqual(gymData.phone)
     expect(createdGym.latitude).toEqual(gymData.latitude)
     expect(createdGym.longitude).toEqual(gymData.longitude)
+  })
+
+  it('should throw ExceptionError when unexpected error occurs', async () => {
+    vi.spyOn(gymsRepository, 'create').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    await expect(() => createGymService.execute({
+      title: 'Gym',
+      description: 'Gym description',
+      phone: '11999999999',
+      latitude: -25.516389,
+      longitude: -49.1798828
+    })).rejects.toBeInstanceOf(ExceptionError)
   })
 })
