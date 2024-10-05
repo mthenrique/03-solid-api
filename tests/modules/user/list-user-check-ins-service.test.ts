88052b7dc9431f79dbda5bfadb7d28cf80fa9068
Mateus Oliveira <mthenriqueo@gmail.com>
2024-10-05 20:57:38 -0300

    test: fix test extension file

--- tests/modules/user/list-user-check-ins-service.test.ts
+++ tests/modules/user/list-user-check-ins-service.test.ts
@@ -1,8 +1,8 @@
-import { CheckInsRepository } from "@/infra/database/repositories/check-ins-repository";
-import { ExceptionError } from "@/infra/errors/exception-error";
-import ListUserCheckInsService from "@/modules/user/services/list-user-check-ins-service";
-import CheckInsRepositoryInMemory from "tests/in-memory-repositories/check-ins-repository-in-memory";
-import { describe, it, expect, beforeEach, vi } from "vitest";
+import { CheckInsRepository } from '@/infra/database/repositories/check-ins-repository'
+import { ExceptionError } from '@/infra/errors/exception-error'
+import ListUserCheckInsService from '@/modules/user/services/list-user-check-ins-service'
+import CheckInsRepositoryInMemory from 'tests/in-memory-repositories/check-ins-repository-in-memory'
+import { describe, it, expect, beforeEach, vi } from 'vitest'
 
 let checkInsRepository: CheckInsRepository
 let checkInsService: ListUserCheckInsService
@@ -16,60 +16,61 @@ describe('ListUserCheckInsService', () => {
   it('should be able to list check-ins by user', async () => {
     await checkInsRepository.create({
       userId: 'user-01',
-      gymId: 'gym-01'
+      gymId: 'gym-01',
     })
 
     const createdCheckIn = await checkInsRepository.create({
       userId: 'user-01',
       gymId: 'gym-02',
-      validatedAt: new Date()
+      validatedAt: new Date(),
     })
 
     const { checkIns } = await checkInsService.execute({
       userId: createdCheckIn.userId,
-      page: 1
+      page: 1,
     })
 
     expect(checkIns).toHaveLength(2)
     expect(checkIns).toEqual([
       expect.objectContaining({ gymId: 'gym-01' }),
-      expect.objectContaining({ gymId: 'gym-02' })
+      expect.objectContaining({ gymId: 'gym-02' }),
     ])
   })
 
-  it('should be able to list paginated check-ins by user'), async () => {
-    console.log('checkInsService:')
-
+  it('should be able to list paginated check-ins by user', async () => {
     for (let i = 1; i <= 22; i++) {
       await checkInsRepository.create({
         userId: 'user-01',
-        gymId: `gym-${i}`
+        gymId: `gym-${i}`,
       })
     }
 
     const { checkIns } = await checkInsService.execute({
       userId: 'user-01',
-      page: 2
+      page: 2,
     })
 
     console.log('checkIns: ', checkIns.length)
 
-    
     expect(checkIns).toHaveLength(2)
     expect(checkIns).toEqual([
       expect.objectContaining({ gymId: 'gym-21' }),
-      expect.objectContaining({ gymId: 'gym-22' })
+      expect.objectContaining({ gymId: 'gym-22' }),
     ])
-  }
+  })
 
   it('should throw ExceptionError when unexpected error occurs', async () => {
-    vi.spyOn(checkInsRepository, 'findManyByUserId').mockImplementationOnce(() => {
-      throw new Error()
-    })
-    
-    await expect(() => checkInsService.execute({
-      userId: 'user-01',
-      page: 1
-    })).rejects.toBeInstanceOf(ExceptionError)
+    vi.spyOn(checkInsRepository, 'findManyByUserId').mockImplementationOnce(
+      () => {
+        throw new Error()
+      },
+    )
+
+    await expect(() =>
+      checkInsService.execute({
+        userId: 'user-01',
+        page: 1,
+      }),
+    ).rejects.toBeInstanceOf(ExceptionError)
   })
 })
